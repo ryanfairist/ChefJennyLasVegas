26585d5d40ba0147a7ea92ef6e9226f4da7ac060
RyanFairist <ryanfairistrichard@gmail.com>
2018-08-15 00:37:49 -0400

    update header component - testing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          <h1 className="App-title">
+            Chef Jenny <br />
+            <span>
+              <i>I love making great food for great people!</i>
+            </span>
+          </h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">Personal Chef at your service.</p>
       </div>
     );
   }
